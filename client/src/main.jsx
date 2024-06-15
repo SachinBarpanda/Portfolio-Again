9707ab27b2223c1210a0b5b10bea229ec41cf56e
Sachin <sachinongithub@gmail.com>
2024-06-16 01:01:35 +0530

    hashRouter added

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,37 +2,43 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { BrowserRouter, HashRouter, Route, RouterProvider, Routes, createBrowserRouter, createHashRouter } from 'react-router-dom'
 import Home from '../pages/Home/Home.jsx'
 import Admin from '../pages/Admin/Admin.jsx'
 import Login from '../pages/Admin/Login.jsx'
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-    children:
-      [
-        {
-          path: '/',
-          element: <Home />,
+// const router = createHashRouter([
+//   {
+//     path: '/',
+//     element: <App />,
+//     children:
+//       [
+//         {
+//           path: '/',
+//           element: <Home />,
 
-        },
-        {
-          path: '/admin',
-          element: <Admin />,
-          errorElement:<h1>Admin ke paas nahi jaa paa raha hai yeh</h1>
-        },
-        {
-          path: '/admin-login',
-          element: <Login />
-        },
-      ]
-  },
-])
+//         },
+//         {
+//           path: '/admin',
+//           element: <Admin />,
+//           errorElement:<h1>Admin ke paas nahi jaa paa raha hai yeh</h1>
+//         },
+//         {
+//           path: '/admin-login',
+//           element: <Login />
+//         },
+//       ]
+//   },
+// ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    {/* <RouterProvider router={router} /> */}
+    <HashRouter>
+        <Route path='/' element={<Home/>} exact/>
+        <Route path='/admin' element={<Admin/>} exact/>
+        <Route path='/admin-login' element={<Login/>} exact/>
+    
+    </HashRouter>
   </React.StrictMode>
 )
